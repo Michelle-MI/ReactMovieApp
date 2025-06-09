01f989a8d798d46c2428d5960b5b8acae8f64e8d
Michelle <muenimichelle21@gmail.com>
2025-06-09 17:37:06 +0300

    Initial commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "1ad3ebd3086bd3d3d3832648e2a0bc4d";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
